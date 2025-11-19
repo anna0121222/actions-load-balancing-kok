79e769329ad0edde6131eddda06e19e6d1a9ce28
anna0121222 <anna012188@gmail.com>
2025-11-19 16:12:50 +0900

    sidebar

--- src/main/resources/static/js/main-page/side-bar/event.js
+++ src/main/resources/static/js/main-page/side-bar/event.js
@@ -455,7 +455,7 @@ sendButton.addEventListener("click", async (e) => {
     const adminNoticeDTOList = await supportResponse.json();
 
     const message = inputTextarea.value;
-    const response = await fetch(`http://localhost:8000/api/question-response`,{
+    const response = await fetch(`http://kok-ok.store/api/question-response`,{
         method: "POST",
         headers: {
             "Content-Type": "application/json"
