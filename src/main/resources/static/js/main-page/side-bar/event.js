dc2f2a683e1740e9279f33caac472dce92bd7dcc
anna0121222 <anna012188@gmail.com>
2025-11-20 14:07:00 +0900

    sidebar

--- src/main/resources/static/js/main-page/side-bar/event.js
+++ src/main/resources/static/js/main-page/side-bar/event.js
@@ -469,7 +469,7 @@ inputTextarea.addEventListener("keydown", async (e) => {
         const message = sendTextContainer();
         console.log(message);
 
-        if(message.value.trim() !== "") {
+        if(message.length > 0 || message.trim() !== "") {
             // ai 응답
             const supportResponse = await fetch(`/api/support/all`);
             const adminNoticeDTOList = await supportResponse.json();
